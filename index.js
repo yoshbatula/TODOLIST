808ef7067c20972cf3bd4429d763900c63e859c5
CCE107 <yoshbatula2@gmail.com>
2024-11-11 13:26:11 +0800

    naa nay javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('todo-form');
+    const input = document.getElementById('todo-btn');
+    const todolist = document.getElementById
+})
